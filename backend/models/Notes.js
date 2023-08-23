f796f6c42d6305779abbe3a2e6438970164ecad3
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-23 22:15:53 +0530

    create two schema (1.NotesSchema, 2.UserSchema)

---
+++ backend/models/Notes.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const NotesSchema = new Schema({
+    title:{
+        type: string,
+        required: true
+    },
+    description:{
+        type: string,
+        required: true,
+        unique:true
+    },
+    tag:{
+        type: string,
+        default: 'General'
+        
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+  });
+
+  module.exports = mongoose.model('notes', NotesSchema)
---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema({
+    name:{
+        type: string,
+        required: true
+    },
+    email:{
+        type: string,
+        required: true,
+        unique:true
+    },
+    password:{
+        type: string,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+  });
+
+  module.exports = mongoose.model('user', UserSchema)
